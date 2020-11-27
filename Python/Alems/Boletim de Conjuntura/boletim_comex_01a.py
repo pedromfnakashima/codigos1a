c983a83c65b2991fbb814d5189fd46ef3dad8c07
Pedro Nakashima <pedromfnakashima@gmail.com>
2020-11-27 11:36:47 -0400

    Boletim alems

--- Python/Alems/Boletim de Conjuntura/boletim_comex_01a.py
+++ Python/Alems/Boletim de Conjuntura/boletim_comex_01a.py
@@ -82,11 +82,11 @@ def mdic_01a(tipo, uf, anos):
 df_exp = mdic_01a(tipo='EXP',uf='RJ', anos=[2020, 2019])
 df_imp = mdic_01a(tipo='IMP',uf='RJ', anos=[2020, 2019])
 
-df_exp_soma = df_exp.groupby(['year','month'])['VL_FOB'].sum()
-df_imp_soma = df_imp.groupby(['year','month'])['VL_FOB'].sum()
+df_exp_somaMeses = df_exp.groupby(['year','month'])['VL_FOB'].sum()
+df_imp_somaMeses = df_imp.groupby(['year','month'])['VL_FOB'].sum()
 
-soma_exp_set2020 = df_exp_soma[2020,9]
-soma_imp_set2020 = df_imp_soma[2020,9]
+soma_exp_set2020 = df_exp_somaMeses[2020,9]
+soma_imp_set2020 = df_imp_somaMeses[2020,9]
 tot_set2020 = soma_exp_set2020 + soma_imp_set2020
 print(f'\nCorrente de comércio em set/2020:')
 print(f'{tot_set2020:,}\n')
@@ -95,8 +95,8 @@ print(f'{soma_exp_set2020:,.2f}\n')
 print(f'Importações set/2020:')
 print(f'{soma_imp_set2020:,.2f}\n')
 
-soma_exp_set2019 = df_exp_soma[2019,9]
-soma_imp_set2019 = df_imp_soma[2019,9]
+soma_exp_set2019 = df_exp_somaMeses[2019,9]
+soma_imp_set2019 = df_imp_somaMeses[2019,9]
 tot_set2019 = soma_exp_set2019 + soma_imp_set2019
 print('=============================')
 
@@ -123,83 +123,144 @@ print(f'{soma_exp_set2019-soma_imp_set2019:,.2f}\n')
 
 print('=============================')
 
-df_exp_soma = df_exp_soma.to_frame().reset_index()
-df_imp_soma = df_imp_soma.to_frame().reset_index()
+df_exp_somaMeses = df_exp_somaMeses.to_frame().reset_index()
+df_imp_somaMeses = df_imp_somaMeses.to_frame().reset_index()
 
-cond1 = df_exp_soma['month'] <= 9
-df_exp_soma = df_exp_soma.loc[cond1, :]
-cond1 = df_imp_soma['month'] <= 9
-df_imp_soma = df_imp_soma.loc[cond1, :]
+cond1 = df_exp_somaMeses['month'] <= 9
+df_exp_somaMeses = df_exp_somaMeses.loc[cond1, :]
+cond1 = df_imp_somaMeses['month'] <= 9
+df_imp_somaMeses = df_imp_somaMeses.loc[cond1, :]
 
-df_exp_soma_anos = df_exp_soma.groupby(['year'])['VL_FOB'].sum()
-df_imp_soma_anos = df_imp_soma.groupby(['year'])['VL_FOB'].sum()
+df_exp_somaAnos = df_exp_somaMeses.groupby(['year'])['VL_FOB'].sum()
+df_imp_somaAnos = df_imp_somaMeses.groupby(['year'])['VL_FOB'].sum()
 
 print(f'\nConta corrente acumulada 2020:')
-soma2020 = df_exp_soma_anos[2020]+df_imp_soma_anos[2020]
+soma2020 = df_exp_somaAnos[2020]+df_imp_somaAnos[2020]
 print(f'{soma2020:,.2f}\n')
 
 print(f'Conta corrente acumulada 2019:')
-soma2019 = df_exp_soma_anos[2019]+df_imp_soma_anos[2019]
+soma2019 = df_exp_somaAnos[2019]+df_imp_somaAnos[2019]
 print(f'{soma2019:,.2f}\n')
 
 print(f'Variação 2019-2020:')
 variacaoB = soma2020-soma2019
 variacaoP = (variacaoB/soma2019)*100
-print(f'{variacao:,.2f} %\n')
+print(f'{variacaoP:,.2f} %\n')
 
 print(f'\nExportações 2020:')
-print(f'{df_exp_soma_anos[2020]:,.2f}\n')
+print(f'{df_exp_somaAnos[2020]:,.2f}\n')
 print(f'Exportações 2019:')
-print(f'{df_exp_soma_anos[2019]:,.2f}\n')
-variacaoB = df_exp_soma_anos[2020]-df_exp_soma_anos[2019]
-variacaoP = (variacaoB/df_exp_soma_anos[2019])*100
+print(f'{df_exp_somaAnos[2019]:,.2f}\n')
+variacaoB = df_exp_somaAnos[2020]-df_exp_somaAnos[2019]
+variacaoP = (variacaoB/df_exp_somaAnos[2019])*100
 print(f'Variação Percentual:')
 print(f'{variacaoP:,.2f} %\n')
 print(f'Variação Bruta:')
 print(f'{variacaoB:,.2f}\n')
 
 print(f'\nImportações 2020:')
-print(f'{df_imp_soma_anos[2020]:,.2f}\n')
+print(f'{df_imp_somaAnos[2020]:,.2f}\n')
 print(f'Importações 2019:')
-print(f'{df_imp_soma_anos[2019]:,.2f}\n')
-variacaoB = df_imp_soma_anos[2020]-df_imp_soma_anos[2019]
-variacaoP = (variacaoB/df_exp_soma_anos[2019])*100
+print(f'{df_imp_somaAnos[2019]:,.2f}\n')
+variacaoB = df_imp_somaAnos[2020]-df_imp_somaAnos[2019]
+variacaoP = (variacaoB/df_exp_somaAnos[2019])*100
 print(f'Variação Percentual:')
 print(f'{variacaoP:,.2f} %\n')
 print(f'Variação Bruta:')
 print(f'{variacaoB:,.2f}\n')
 
-saldocomercial = df_exp_soma_anos[2020]-df_imp_soma_anos[2020]
+saldocomercial = df_exp_somaAnos[2020]-df_imp_somaAnos[2020]
 print(f'Saldo comercial 2020:')
 print(f'{saldocomercial:,.2f}\n')
 
+print('=============================')
+print('=============================')
+print('=============================')
 
-df_exp_soma_anos = df_exp_soma[2020,9]
-df_imp_soma_anos = df_imp_soma[2020,9]
+pasta = caminho_base / 'Dados' / 'mdic' / 'Tabelas - classificações'
+fatorAgregado = pd.read_excel(pasta/'TABELAS_AUXILIARES.xlsx', sheet_name='7')
+print(fatorAgregado['NO_FAT_AGREG'].value_counts())
+print(fatorAgregado['NO_FAT_AGREG_GP'].value_counts())
+
+df_exp_fatorAgregado = df_exp.merge(fatorAgregado,how='left',left_on='CO_NCM',right_on='CO_NCM')
+
+cond1 = df_exp_fatorAgregado['month'] <= 9
+df_exp_fatorAgregado = df_exp_fatorAgregado.loc[cond1,:]
+
+df_exp_fatorAgregado_fatAgregGP_somaAnos = df_exp_fatorAgregado.groupby(['year','NO_FAT_AGREG_GP'])['VL_FOB'].sum()
+print(f'\nProdutos industrializados 2020:')
+print(f'{df_exp_fatorAgregado_fatAgregGP_somaAnos[2020,"PRODUTOS INDUSTRIALIZADOS"]:,.2f}\n')
+
+df_exp_fatorAgregado_fatAgreg_somaAnos = df_exp_fatorAgregado.groupby(['year','NO_FAT_AGREG'])['VL_FOB'].sum()
+print(f'\nProdutos industrializados 2020:')
+print(f'{df_exp_fatorAgregado_fatAgreg_somaAnos[2020,"PRODUTOS SEMIMANUFATURADOS"]:,.2f}\n')
+
+cond1 = df_exp_fatorAgregado['NO_NCM_POR'].str.contains('máquinas')
+filtro = df_exp_fatorAgregado.loc[cond1,:]
+
+print('=============================')
+
+pasta = caminho_base / 'Dados' / 'mdic' / 'Tabelas - classificações'
+fatorAgregado = pd.read_csv(pasta/'NCM.csv')
+
+ncmNomes = pd.read_csv(pasta / 'NCM.csv',
+                       encoding = 'latin',
+                       delimiter = ';')
+
+ncmNomes = ncmNomes[['CO_NCM','NO_NCM_POR']]
+
+cond1 = ncmNomes['NO_NCM_POR'].str.contains('coque')
+filtro = ncmNomes.loc[cond1,:]
+
+print('=============================')
+
+pasta = caminho_base / 'Dados' / 'mdic' / 'Tabelas - classificações'
+isic = pd.read_excel(pasta/'TABELAS_AUXILIARES.xlsx', sheet_name='4')
+
+isic = isic[['CO_NCM', 'NO_ISIC_DIVISAO']]
+
+df_exp_fatorAgregado_isic = df_exp_fatorAgregado.merge(isic,how='left',left_on='CO_NCM',right_on='CO_NCM')
+
+print(df_exp_fatorAgregado_isic['month'].value_counts())
+
+df_exp_fatorAgregado_isic_somaAnos = df_exp_fatorAgregado_isic.groupby(['year','NO_ISIC_DIVISAO'])['VL_FOB'].sum().to_frame().reset_index()
+
+df_exp_fatorAgregado_isic_somaAnos.sort_values(by=['year','VL_FOB'], ascending=[False,False], inplace=True)
+
+df_exp_fatorAgregado_isic_soma2019 = df_exp_fatorAgregado_isic_somaAnos.loc[df_exp_fatorAgregado_isic_somaAnos['year']==2019,:]
+df_exp_fatorAgregado_isic_soma2020 = df_exp_fatorAgregado_isic_somaAnos.loc[df_exp_fatorAgregado_isic_somaAnos['year']==2020,:]
+df_exp_fatorAgregado_isic_soma2019.rename(columns={'VL_FOB':'2019'},inplace=True)
+df_exp_fatorAgregado_isic_soma2020.rename(columns={'VL_FOB':'2020'},inplace=True)
+
+df_exp_fatorAgregado_isic_somaAnos2 = df_exp_fatorAgregado_isic_soma2019.merge(df_exp_fatorAgregado_isic_soma2020,how='left',left_on='NO_ISIC_DIVISAO',right_on='NO_ISIC_DIVISAO')
+
+df_exp_fatorAgregado_isic_somaAnos2['variação'] = df_exp_fatorAgregado_isic_somaAnos2['2020'] - df_exp_fatorAgregado_isic_somaAnos2['2019']
+
+df_exp_fatorAgregado_isic_somaAnos2.sort_values(by=['variação'], inplace=True)
+
+df_exp_fatorAgregado_isic_somaAnos2['variaçãoP'] = (df_exp_fatorAgregado_isic_somaAnos2['variação'] / df_exp_fatorAgregado_isic_somaAnos2['2019'])*100
+
+vl_coque = df_exp_fatorAgregado_isic_somaAnos2.loc[3,'variaçãoP']
+vl_transp = df_exp_fatorAgregado_isic_somaAnos2.loc[2,'variaçãoP']
+
+print(f'{vl_coque:,.2f}\n')
+print(f'{vl_transp:,.2f}\n')
+
+
+########################################################################
+############################## IMPORTAÇÕES #############################
+########################################################################
+
+
+
+
+
+
 
 
 
 print(f'{:,.2f}\n')
-
-#corrente_2019 = df_exp_soma[2019] + df_imp_soma[2019]
-#corrente_2020 = df_exp_soma[2020] + df_imp_soma[2020]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f'{:,.2f}\n')
 
 
 
