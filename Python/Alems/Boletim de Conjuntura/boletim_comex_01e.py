9451a9b7b9b388e08c1037341d4c1262a788f25e
Pedro Nakashima <pedromfnakashima@gmail.com>
2020-11-30 12:56:08 -0400

    Comex Alems

--- Python/Alems/Boletim de Conjuntura/boletim_comex_01e.py
+++ Python/Alems/Boletim de Conjuntura/boletim_comex_01e.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 29 14:06:27 2020
+Created on Mon Nov 30 08:15:25 2020
 
-@author: pedro
+@author: pedro-salj
 """
 
 globals().clear()
@@ -24,7 +24,20 @@ import pandas as pd
 ##########################################################################################################
 ##########################################################################################################
 
-def mdic_01a(tipo, uf, anos):
+########################################################################################
+############ Exportações do Estado segundo as principais indústrias ####################
+########################################################################################
+
+
+
+
+
+
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+
+def mdic_01c(tipo, uf, anos):
     for index_ano, ano in enumerate(anos):
         arq_nome = tipo + '_' + str(ano) + '.csv'
         
@@ -48,8 +61,8 @@ def mdic_01a(tipo, uf, anos):
     df_bruto.index = range(len(df_bruto))
     if tipo == 'EXP':
         pasta = caminho_base / 'Dados' / 'mdic' / 'Tabelas - classificações'
-        catsExp = pd.read_excel(pasta/'TABELAS_AUXILIARES.xlsx', sheet_name='7')
-        catsExp = catsExp[['CO_NCM','NO_FAT_AGREG']]
+        catsExp = pd.read_excel(pasta/'TABELAS_AUXILIARES.xlsx', sheet_name='4')
+        catsExp = catsExp[['CO_NCM','NO_ISIC_DIVISAO','NO_ISIC_SECAO']]
         df_bruto = df_bruto.merge(catsExp,how='left',left_on='CO_NCM',right_on='CO_NCM')
     elif  tipo == 'IMP':
         pasta = caminho_base / 'Dados' / 'mdic' / 'Tabelas - classificações'
@@ -57,14 +70,91 @@ def mdic_01a(tipo, uf, anos):
         catsImp = catsImp[['CO_NCM','NO_CGCE_N1','NO_CGCE_N3']]
         df_bruto = df_bruto.merge(catsImp,how='left',left_on='CO_NCM',right_on='CO_NCM')
     return df_bruto
+########################################################################################
 
-def mdic_01b(uf, anos, milhões=False):
+dicionário = {}
+
+
+
+df_exp = mdic_01c(tipo='EXP',uf='RJ', anos=[2018,2019,2020])
+#df_exp = mdic_01c(tipo='EXP',uf='MS', anos=[2018,2019,2020])
+
+meses = df_exp.groupby(['dt']).head(1)['dt'].to_frame().set_index('dt')
+meses.index.freq = 'MS'
+
+# 
+soma = df_exp.groupby(['dt','NO_ISIC_DIVISAO'])['VL_FOB'].sum().to_frame()
+soma.reset_index(inplace=True)
+unicos = list(soma['NO_ISIC_DIVISAO'].unique())
+for unico in unicos:
+    print(unico)
+
+unico = 'Atividades criativas, artes e entretenimento'
+index_unico = 0
+
+unico = 'Extração de petróleo bruto e gás natural'
+index_unico = 1
+print(unico)
+
+cond1 = soma['NO_ISIC_DIVISAO'] == unico
+filtro = soma.loc[cond1,['dt','VL_FOB']]
+filtro.rename(mapper={'VL_FOB':unico},axis=1,inplace=True)
+filtro.set_index('dt',inplace=True)
+
+meses_cp = meses.copy()
+meses_cp = meses_cp.merge(filtro,how='left',left_index=True,right_index=True)
+meses_cp.fillna(0, inplace=True)
+
+if index_unico == 0:
+    dicionário['Mensal - Valor'] = meses_cp
+else:
+    dicionário['Mensal - Valor'] = dicionário['Mensal - Valor'].merge(meses_cp,how='left',left_index=True,right_index=True)
+
+
+
+'Mensal - Valor'
+'Acumulado no Ano - Valor'
+'Acumulado em 12 meses - Valor'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+soma.rename(mapper={'VL_FOB':'Exportações'},axis=1,inplace=True)
+df_mensal = soma.copy()
+
+
+
+
+
+
+
+
+
+
+
+
+def mdic_01d(uf, anos, milhões=False):
     dicionário = {}
     #################################################################################
     ########## Mensal - Valor #######################################################
     #################################################################################
-    df_exp = mdic_01a(tipo='EXP',uf=uf, anos=anos)
-    df_imp = mdic_01a(tipo='IMP',uf=uf, anos=anos)
+    df_exp = mdic_01c(tipo='EXP',uf=uf, anos=anos)
+    df_imp = mdic_01c(tipo='IMP',uf=uf, anos=anos)
     # EXPORTAÇÕES TOTAIS MENSAIS
     soma = df_exp.groupby(['dt'])['VL_FOB'].sum().to_frame()
     soma.rename(mapper={'VL_FOB':'Exportações'},axis=1,inplace=True)
@@ -335,13 +425,10 @@ def mdic_01b(uf, anos, milhões=False):
 ######################################################################################################
 ######################################################################################################
 
-tabMdicMS = mdic_01b('MS', anos=[2018,2019,2020], milhões=True)
 
-tabMdicRJ = mdic_01b('RJ', anos=[2018,2019,2020], milhões=True)
+tabMdicRJ = mdic_01d('RJ', anos=[2018,2019,2020], milhões=True)
 
-tabMdicSP = mdic_01b('SP', anos=[2018,2019,2020], milhões=True)
 
-tabMdicBR = mdic_01b('BR', anos=[2018,2019,2020], milhões=True)
 
 ######################################################################################################
 ######################################################################################################
@@ -350,172 +437,6 @@ tabMdicBR = mdic_01b('BR', anos=[2018,2019,2020], milhões=True)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-df_mensal = tabMdicSP['Mensal - Valor'].copy()
-
-df_mensal.fillna(0, inplace=True)
-
-
-
-
-
-
-
-
-
-
-df_acum12m = tabMdicMS['Mensal - Valor'].copy()
-colunas = list(df_acum12m.columns)
-for coluna in colunas:
-    print(coluna)
-    df_acum12m[coluna] = df_acum12m[coluna].rolling(12).sum()
-
-
-df_acum12m.dropna(thresh=1, inplace=True)
-#     --->>>> Muda a frequência do index para MS <<<<< ---   #
-df_acum12m.index.freq = 'MS'
-#     --->>>> Coloca no dicionário <<<<< ---   #
-dicionário['Acumulado em 12 meses - Valor'] = df_acum12m
-
-
-
-
-
-
-
-tabMdic_cp = tabMdic['Mensal - Valor'].copy()
-tabMdic_cp_L12 = tabMdic_cp.shift(periods=12)
-colunas = list(tabMdic_cp.columns)
-for coluna in colunas:
-    print(coluna)
-    tabMdic_cp[coluna] = ((tabMdic_cp[coluna] - tabMdic_cp_L12[coluna]) / tabMdic_cp_L12[coluna]) * 100
-tabMdic_cp.dropna(inplace=True)
-
-
-df.shift(periods=3)
-
-
-print(tabMdic_cp.index)
-
-colunas = list(tabMdic_cp.columns)
-for coluna in colunas:
-    tabMdic_cp[coluna] = tabMdic_cp[coluna].rolling(12).sum()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-tabMdic_cp = tabMdic['Acumulado em 12 meses - Participação'].copy()
-tabMdic_cp.drop(['Exportações','Importações'],axis=1,inplace=True)
-tabMdic_cp.dropna(inplace=True)
-
-
-tabMdic_cp = tabMdic['Valor Mensal'].copy() / 1_000_000
-
-colunas = pd.Series(tabMdic_cp.columns)
-cond1 = colunas.str.contains('Imp\.')
-colunas = list(colunas[cond1])
-for coluna in colunas:
-    print(coluna)
-    tabMdic_cp[coluna] = (tabMdic_cp[coluna] / tabMdic_cp['Importações']) * 100
-
-colunas = pd.Series(tabMdic_cp.columns)
-cond1 = colunas.str.contains('Exp\.')
-colunas = list(colunas[cond1])
-for coluna in colunas:
-    print(coluna)
-    tabMdic_cp[coluna] = (tabMdic_cp[coluna] / tabMdic_cp['Exportações']) * 100
-    
-
-['Saldo Comercial','Corrente de Comércio']
-
-str1 = 'abc'
-print('d' in str1)
-
-
-
-
-del bool
-
-
-
-
-tabMdic_cp = tabMdic['Valor Mensal'].copy() / 1_000_000
-colunas = list(tabMdic_cp.columns)
-for coluna in colunas:
-    tabMdic_cp[coluna] = tabMdic_cp[coluna].rolling(12).sum()
-
-
-
-tabMdic_cp = tabMdic['Valor Mensal'].copy() / 1_000_000
-tabMdic_cp['dt_ano'] = bcnd.index.year
-colunas = list(set(list(tabMdic_cp.columns)) - {'dt_ano'})
-for coluna in colunas:
-    print(coluna)
-    tabMdic_cp[coluna] = tabMdic_cp.groupby(['dt_ano'])[coluna].cumsum()
-tabMdic_cp.drop(['dt_ano'],axis=1,inplace=True)
-
-
-
-
-
-
-
-
-
-
-tabMdic_cp = tabMdic_cp.groupby(['dt_ano'])['Imp. Bens de Consumo não-duráveis'].cumsum()
-
-
-bcnd = tabMdic_cp['Imp. Bens de Consumo não-duráveis'].to_frame()
-bcnd['dt_ano'] = bcnd.index.year
-
-bcnd['cumsum'] = bcnd.groupby(['dt_ano'])['Imp. Bens de Consumo não-duráveis'].cumsum()
-
-
-
-print(tabMdic_cp.columns)
-
-
-
-pasta = caminho_base / 'Dados' / 'alems' / 'Boletim de conjuntura'
-with pd.ExcelWriter(pasta / 'Dados_01a.xlsx', mode='a', engine="openpyxl") as writer:  
-    bcnd.to_excel(writer, sheet_name='planTeste1', index=True)
 
 
 
