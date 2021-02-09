28b86aec0d179782dd69a61152e43bedcd68ac47
Pedro Nakashima <pedromfnakashima@gmail.com>
2021-02-09 12:30:19 -0400

    json-server

---
+++ Cursos e Vídeos/JavaScript/Curso completo do iniciante ao mestre - Daniel Morales (udemy)/306AJAX/310todolist/js/createXMLHttpRequest.js
@@ -0,0 +1,26 @@
+export function createXMLHttpRequest(method, url, cb, data = null) {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.send(data);
+
+  xhr.onreadystatechange = verificaAjax;
+
+  function verificaAjax() {
+    if (xhr.readyState === 4) {
+      if (xhr.status < 400) {
+        const json = JSON.parse(xhr.responseText);
+
+        if (typeof cb === 'function') {
+          cb(json);
+        }
+      } else if (typeof cb === 'function') {
+        cb({
+          error: true,
+          status: xhr.status,
+          message: 'algo deu errado com a conexão',
+        });
+      }
+    }
+  }
+}
