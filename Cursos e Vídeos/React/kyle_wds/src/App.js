2fc140e82a1bc3c75e43193ee8a6c80b836a0126
Pedro Nakashima <pedromfnakashima@gmail.com>
2021-03-06 11:02:00 -0400

    Cooking-With-React

--- Cursos e Vídeos/React/kyle_wds/src/App.js
+++ Cursos e Vídeos/React/kyle_wds/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
+import CounterHooks from './CounterHooks';
+
+export const ThemeContext = React.createContext();
 
 function App() {
-  return <Counter initialCount={0} />;
+  console.log('Render App');
+  const [theme, setTheme] = useState('red');
+  return (
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
+      Counter
+      <Counter initialCount={0} />
+      Counter Hooks
+      <CounterHooks initialCount={0} />
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === 'red' ? 'blue' : 'red';
+          })
+        }
+      >
+        {' '}
+        Toggle Theme{' '}
+      </button>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
--- Cursos e Vídeos/React/kyle_wds/src/Counter.js
+++ Cursos e Vídeos/React/kyle_wds/src/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ThemeContext } from './App';
 
 export default class Counter extends Component {
   constructor(props) {
@@ -10,12 +11,21 @@ export default class Counter extends Component {
   }
 
   render() {
+    console.log('Render Counter');
     return (
-      <div>
-        <button onClick={() => this.changeCount(-1)}>-</button>
-        <span>{this.state.count}</span>
-        <button onClick={() => this.changeCount(1)}>+</button>
-      </div>
+      <ThemeContext.Consumer>
+        {(style) => (
+          <div>
+            <button style={style} onClick={() => this.changeCount(-1)}>
+              -
+            </button>
+            <span>{this.state.count}</span>
+            <button style={style} onClick={() => this.changeCount(1)}>
+              +
+            </button>
+          </div>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 
